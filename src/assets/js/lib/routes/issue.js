2b7edba9481e8070c7a132d338b8e241a707e0df
puri <puripant@gmail.com>
2017-01-14 23:06:14 +0700

    Use the populated owner field in issue modal instead of calling users api

--- src/assets/js/lib/routes/issue.js
+++ src/assets/js/lib/routes/issue.js
@@ -29,256 +29,247 @@ const issueRouter = module.exports = {
       })
       .then(response => response.json())
       .then(data => {
-        fetch(util.site_url('/users/' + data.owner, app.config.api_url), {
-          method: 'GET',
-          headers: {
-            'Content-type': 'application/json',
-            Authorization: 'Bearer ' + user.token
-          }
-        })
-        .then(response => response.json())
-        .then(owner => {
-          data.photos.forEach(d =>
-            $('.slides').append('<li><img class="materialboxed" src=' + d + '></li>')
-          );
+        const owner = data.owner;
+        data.photos.forEach(d =>
+          $('.slides').append('<li><img class="materialboxed" src=' + d + '></li>')
+        );
 
-          const $reporter = $('#reporter');
-          const $span = $reporter.find('span');
-          $span.eq(0).text(owner.name);
-          $span.eq(1).text((new Date(data.created_time)).toLocaleDateString());
-          $reporter.find('a.btn-flat').attr('href', 'mailto:' + owner.email);
+        const $reporter = $('#reporter');
+        const $span = $reporter.find('span');
+        $span.eq(0).text(owner.name);
+        $span.eq(1).text((new Date(data.created_time)).toLocaleDateString());
+        $reporter.find('a.btn-flat').attr('href', 'mailto:' + owner.email);
 
-          const $details = $('#details');
-          $details.find('textarea')
-            .val(data.detail)
-            .trigger('autoresize');
+        const $details = $('#details');
+        $details.find('textarea')
+          .val(data.detail)
+          .trigger('autoresize');
 
-          $('.chips').material_chip({
-            placeholder: 'Enter a tag',
-            secondaryPlaceholder: 'Enter a tag'
+        $('.chips').material_chip({
+          placeholder: 'Enter a tag',
+          secondaryPlaceholder: 'Enter a tag'
+        });
+
+        const $chips = $details.find('.chips-initial');
+        $chips.eq(0).material_chip({ data: data.categories.map(d => ({ tag: d })) });
+        $chips.eq(1).material_chip({ data: data.location.coordinates.map(d => ({ tag: d })) });
+        $chips.eq(2).material_chip({ data: data.tags.map(d => ({ tag: d })) });
+
+        // Disable chips aka tags when the user role is non-admin
+        if (!user.is_superuser) {
+          $chips.find('i').remove();
+          $chips.find('input')
+            .attr('placeholder', '')
+            .prop('disabled', true);
+        }
+
+        // Dropdown lists
+        const $status = $('#status');
+        const $select = $status.find('select');
+
+        // Disable dropdown when the user role is non-admin
+        if (!user.is_superuser) {
+          $status.find('input')
+            .prop('disabled', true);
+        }
+
+        // Populate department dropdown list
+        const $select_department = $select.eq(1);
+        api.getDepartments()
+        .then(departments => {
+          $select_department.empty();
+          $select_department.append('<option value="">[Please select]</option>');
+          departments.data.forEach(department => {
+            $select_department.append('<option value="' + department._id + '">' +
+              department.name + '</option>');
           });
-
-          const $chips = $details.find('.chips-initial');
-          $chips.eq(0).material_chip({ data: data.categories.map(d => ({ tag: d })) });
-          $chips.eq(1).material_chip({ data: data.location.coordinates.map(d => ({ tag: d })) });
-          $chips.eq(2).material_chip({ data: data.tags.map(d => ({ tag: d })) });
-
-          // Disable chips aka tags when the user role is non-admin
-          if (!user.is_superuser) {
-            $chips.find('i').remove();
-            $chips.find('input')
-              .attr('placeholder', '')
-              .prop('disabled', true);
+          if (data.assigned_department !== undefined && data.assigned_department !== '') {
+            $select_department.val(data.assigned_department);
           }
+          $select_department.material_select();
+        });
 
-          // Dropdown lists
-          const $status = $('#status');
-          const $select = $status.find('select');
+        // update progress feed UI
+        data.progresses.forEach((progress) =>
+          prependProgressCard({
+            date: progress.created_time,
+            description: progress.detail,
+            url: progress.photos[0]
+          })
+        );
 
-          // Disable dropdown when the user role is non-admin
-          if (!user.is_superuser) {
-            $status.find('input')
-              .prop('disabled', true);
-          }
+        // Init Materialize
+        $('.slider').slider({ height: $('.slider img').width() });
+        $('.slider').slider('pause');
+        $('.materialboxed').materialbox();
 
-          // Populate department dropdown list
-          const $select_department = $select.eq(1);
-          api.getDepartments()
-          .then(departments => {
-            $select_department.empty();
-            $select_department.append('<option value="">[Please select]</option>');
-            departments.data.forEach(department => {
-              $select_department.append('<option value="' + department._id + '">' +
-                department.name + '</option>');
-            });
-            if (data.assigned_department !== undefined && data.assigned_department !== '') {
-              $select_department.val(data.assigned_department);
+        // Buttons
+        $('#cancel').click(() => {
+          $('#manage-issue-modal').modal('close');
+        });
+        $('#confirm').click(() => {
+          const body = {
+            owner: user._id,
+            detail: $details.find('textarea').val(),
+            categories: $chips.eq(0).material_chip('data').map(d => d.tag),
+            location: {
+              coordinates: $chips.eq(1).material_chip('data').map(d => d.tag),
+              type: 'Point'
+            },
+            tags: $chips.eq(2).material_chip('data').map(d => d.tag),
+            assigned_department: $select_department.val()
+          };
+          /* $select.eq(0).val(data.status.priority);
+          $status.find('textarea').val(data.status.annotation)*/
+
+          // Edit pin info (partially)
+          api.patchPin(id, body)
+          .then(response => response.json())
+          .then(() => $('#manage-issue-modal').modal('close'))
+          .catch(err =>
+            Materialize.toast(err.message, 8000, 'dialog-error large')
+          );
+        });
+
+        const $archive = $('#archive');
+        if (user.is_superuser && (data.status === 'rejected' || data.status === 'resolved')) {
+          $archive.show();
+        } else {
+          $archive.hide();
+        }
+        $archive.click(() => {
+          api.patchPin(id, {
+            is_archived: true
+          })
+          .then(response => response.json())
+          .then(() => $('#manage-issue-modal').modal('close'))
+          .catch(err =>
+            Materialize.toast(err.message, 8000, 'dialog-error large')
+          );
+        });
+
+        const $reject = $('#reject');
+        if ((user.is_superuser && (data.status === 'unverified' || data.status === 'verified')) ||
+            (!user.is_superuser && data.status === 'assigned')) {
+          $reject.show();
+        } else {
+          $reject.hide();
+        }
+        $reject.click(() => {
+          api.postTransition(id, {
+            state: 'rejected'
+          })
+          .then(response => response.json())
+          .then(() => $('#manage-issue-modal').modal('close'))
+          .catch(err =>
+            Materialize.toast(err.message, 8000, 'dialog-error large')
+          );
+        });
+
+        $('#goToNextState')
+          .text(() => {
+            switch (data.status) {
+              case 'unverified':
+                return 'Verify';
+              case 'verified':
+                return 'Assign';
+              case 'assigned':
+                return user.is_superuser ? 'Process' : 'Accept';
+              case 'processing':
+                return 'Resolve';
+              case 'resolved':
+                return 'Process';
+              default:
+                return 'Recover';
             }
-            $select_department.material_select();
+          })
+          .click(() => {
+            let body;
+            switch (data.status) {
+              case 'unverified':
+              default:
+                body = {
+                  state: 'verified'
+                };
+                break;
+              case 'verified':
+                body = {
+                  state: 'assigned',
+                  assigned_department: $select_department.val()
+                };
+                break;
+              case 'assigned':
+              case 'resolved':
+                body = {
+                  state: 'processing',
+                  processed_by: user._id
+                };
+                break;
+              case 'processing':
+                body = {
+                  state: 'resolved'
+                };
+                break;
+            }
+
+            if (data.status === 'verified' && $select_department.val() === '') {
+              Materialize.toast('Please select a department', 8000, 'dialog-error large');
+            } else {
+              api.postTransition(id, body)
+              .then(response => response.json())
+              .then(() => $('#manage-issue-modal').modal('close'))
+              .catch(err => {
+                Materialize.toast(err.message, 8000, 'dialog-error large');
+              });
+            }
           });
 
-          // update progress feed UI
-          data.progresses.forEach((progress) =>
-            prependProgressCard({
-              date: progress.created_time,
-              description: progress.detail,
-              url: progress.photos[0]
-            })
-          );
-
-          // Init Materialize
-          $('.slider').slider({ height: $('.slider img').width() });
-          $('.slider').slider('pause');
-          $('.materialboxed').materialbox();
-
-          // Buttons
-          $('#cancel').click(() => {
-            $('#manage-issue-modal').modal('close');
-          });
-          $('#confirm').click(() => {
-            const body = {
-              owner: user._id,
-              detail: $details.find('textarea').val(),
-              categories: $chips.eq(0).material_chip('data').map(d => d.tag),
-              location: {
-                coordinates: $chips.eq(1).material_chip('data').map(d => d.tag),
-                type: 'Point'
-              },
-              tags: $chips.eq(2).material_chip('data').map(d => d.tag),
-              assigned_department: $select_department.val()
+        // Area to update issue progress
+        const $progress = $('#progress');
+        // Disable progress if the issue is not accepted
+        const isAssigned = (data.status === 'assigned');
+        $progress.find('textarea, input').prop('disabled', isAssigned);
+        $progress.find('a').toggleClass('disabled', isAssigned);
+        // Set Post button event
+        $('#post').click(() => {
+          const files = $progress.find('input[type="file"]')[0].files;
+          if (files.length > 0) {
+            const progressData = {
+              photos: [
+                window.URL.createObjectURL(files[0])
+              ],
+              detail: $progress.find('textarea').val()
             };
-            /* $select.eq(0).val(data.status.priority);
-            $status.find('textarea').val(data.status.annotation)*/
+
+            // update progress feed UI
+            prependProgressCard({
+              date: new Date(),
+              description: progressData.detail,
+              url: progressData.photos[0]
+            });
+            $('.materialboxed').materialbox();
 
             // Edit pin info (partially)
-            api.patchPin(id, body)
-            .then(response => response.json())
-            .then(() => $('#manage-issue-modal').modal('close'))
-            .catch(err =>
-              Materialize.toast(err.message, 8000, 'dialog-error large')
-            );
-          });
-
-          const $archive = $('#archive');
-          if (user.is_superuser && (data.status === 'rejected' || data.status === 'resolved')) {
-            $archive.show();
-          } else {
-            $archive.hide();
-          }
-          $archive.click(() => {
-            api.patchPin(id, {
-              is_archived: true
-            })
-            .then(response => response.json())
-            .then(() => $('#manage-issue-modal').modal('close'))
-            .catch(err =>
-              Materialize.toast(err.message, 8000, 'dialog-error large')
-            );
-          });
-
-          const $reject = $('#reject');
-          if ((user.is_superuser && (data.status === 'unverified' || data.status === 'verified')) ||
-              (!user.is_superuser && data.status === 'assigned')) {
-            $reject.show();
-          } else {
-            $reject.hide();
-          }
-          $reject.click(() => {
-            api.postTransition(id, {
-              state: 'rejected'
-            })
-            .then(response => response.json())
-            .then(() => $('#manage-issue-modal').modal('close'))
-            .catch(err =>
-              Materialize.toast(err.message, 8000, 'dialog-error large')
-            );
-          });
-
-          $('#goToNextState')
-            .text(() => {
-              switch (data.status) {
-                case 'unverified':
-                  return 'Verify';
-                case 'verified':
-                  return 'Assign';
-                case 'assigned':
-                  return user.is_superuser ? 'Process' : 'Accept';
-                case 'processing':
-                  return 'Resolve';
-                case 'resolved':
-                  return 'Process';
-                default:
-                  return 'Recover';
-              }
-            })
-            .click(() => {
-              let body;
-              switch (data.status) {
-                case 'unverified':
-                default:
-                  body = {
-                    state: 'verified'
-                  };
-                  break;
-                case 'verified':
-                  body = {
-                    state: 'assigned',
-                    assigned_department: $select_department.val()
-                  };
-                  break;
-                case 'assigned':
-                case 'resolved':
-                  body = {
-                    state: 'processing',
-                    processed_by: user._id
-                  };
-                  break;
-                case 'processing':
-                  body = {
-                    state: 'resolved'
-                  };
-                  break;
-              }
-
-              if (data.status === 'verified' && $select_department.val() === '') {
-                Materialize.toast('Please select a department', 8000, 'dialog-error large');
-              } else {
-                api.postTransition(id, body)
-                .then(response => response.json())
-                .then(() => $('#manage-issue-modal').modal('close'))
-                .catch(err => {
-                  Materialize.toast(err.message, 8000, 'dialog-error large');
-                });
-              }
+            const form = new FormData();
+            fetch(progressData.photos[0])
+            .then(response => response.blob())
+            .then(blob => {
+              form.append('image', blob);
+              api.postPhoto(form)
+              .then(response => response.json())
+              .then(photo_data => {
+                progressData.photos[0] = photo_data.url;
+                api.patchPin(id, {
+                  owner: user._id,
+                  $push: { progresses: progressData }
+                })
+                // .then(response => response.json())
+                .catch(err =>
+                  Materialize.toast(err.message, 8000, 'dialog-error large')
+                );
+              });
             });
-
-          // Area to update issue progress
-          const $progress = $('#progress');
-          // Disable progress if the issue is not accepted
-          const isAssigned = (data.status === 'assigned');
-          $progress.find('textarea, input').prop('disabled', isAssigned);
-          $progress.find('a').toggleClass('disabled', isAssigned);
-          // Set Post button event
-          $('#post').click(() => {
-            const files = $progress.find('input[type="file"]')[0].files;
-            if (files.length > 0) {
-              const progressData = {
-                photos: [
-                  window.URL.createObjectURL(files[0])
-                ],
-                detail: $progress.find('textarea').val()
-              };
-
-              // update progress feed UI
-              prependProgressCard({
-                date: new Date(),
-                description: progressData.detail,
-                url: progressData.photos[0]
-              });
-              $('.materialboxed').materialbox();
-
-              // Edit pin info (partially)
-              const form = new FormData();
-              fetch(progressData.photos[0])
-              .then(response => response.blob())
-              .then(blob => {
-                form.append('image', blob);
-                api.postPhoto(form)
-                .then(response => response.json())
-                .then(photo_data => {
-                  progressData.photos[0] = photo_data.url;
-                  api.patchPin(id, {
-                    owner: user._id,
-                    $push: { progresses: progressData }
-                  })
-                  // .then(response => response.json())
-                  .catch(err =>
-                    Materialize.toast(err.message, 8000, 'dialog-error large')
-                  );
-                });
-              });
-            }
-          });
+          }
         });
       });
     }
